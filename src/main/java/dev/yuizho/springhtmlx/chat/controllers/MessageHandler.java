17558d15e520d136f3fe05d568954368326a7da4
yuizho <yuizho3@gmail.com>
2025-11-15 14:31:10 +0900

    modify chat

--- src/main/java/dev/yuizho/springhtmlx/chat/controllers/MessageHandler.java
+++ src/main/java/dev/yuizho/springhtmlx/chat/controllers/MessageHandler.java
@@ -3,6 +3,7 @@ package dev.yuizho.springhtmlx.chat.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -12,12 +13,28 @@ import java.util.concurrent.ConcurrentMap;
 
 public class MessageHandler extends TextWebSocketHandler {
     private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
-    ConcurrentMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private ConcurrentMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.putIfAbsent(session.getId(), session);
+        var response = new TextMessage(
+                String.format(
+                        "<ul hx-swap-oob=\"beforeend:#histories\"><li>%sが入出しました！</li></ul>",
+                        session.getId()
+                )
+        );
+
+        // ブロードキャスティング
+        for (var s : sessions.values()) {
+            if (s.isOpen()) {
+                s.sendMessage(response);
+            }
+        }
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        sessions.putIfAbsent(session.getId(), session);
-
         var payload = message.getPayload();
         logger.info("Received message: {}", payload);
 
@@ -30,10 +47,29 @@ public class MessageHandler extends TextWebSocketHandler {
                         """
                                 <ul hx-swap-oob=\"beforeend:#histories\"><li>%sからメッセージが届きました！</li></ul>
                                 
-                                <ul hx-swap-oob=\"beforeend:#messages\"><li>%s</li></ul>
+                                <ul hx-swap-oob=\"beforeend:#messages\"><li>%s: %s</li></ul>
                                 """,
                         session.getId(),
+                        session.getId(),
                         content
+                )
+        );
+
+        // ブロードキャスティング
+        for (var s : sessions.values()) {
+            if (s.isOpen()) {
+                s.sendMessage(response);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId());
+        var response = new TextMessage(
+                String.format(
+                        "<ul hx-swap-oob=\"beforeend:#histories\"><li>%sが退出しました！</li></ul>",
+                        session.getId()
                 )
         );
 
