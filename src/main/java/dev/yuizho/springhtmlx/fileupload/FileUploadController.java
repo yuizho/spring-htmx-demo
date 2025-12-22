40f81851ad57e58dda48fcda9f53714064f0b7cb
yuizho <yuizho3@gmail.com>
2025-12-22 23:43:42 +0900

    refactor FileUploadController

--- src/main/java/dev/yuizho/springhtmlx/fileupload/FileUploadController.java
+++ src/main/java/dev/yuizho/springhtmlx/fileupload/FileUploadController.java
@@ -1,7 +1,6 @@
 package dev.yuizho.springhtmlx.fileupload;
 
 import dev.yuizho.springhtmlx.configs.FileUploadProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
