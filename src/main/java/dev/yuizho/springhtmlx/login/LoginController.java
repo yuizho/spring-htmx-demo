ef986695c823ecbd35be505b2cc0ee18004ef899
yuizho <yuizho3@gmail.com>
2026-01-02 21:34:00 +0900

    refactor login feature

--- src/main/java/dev/yuizho/springhtmlx/login/LoginController.java
+++ src/main/java/dev/yuizho/springhtmlx/login/LoginController.java
@@ -1,19 +1,16 @@
 package dev.yuizho.springhtmlx.login;
 
 import com.github.javafaker.Faker;
-import dev.yuizho.springhtmlx.feed.Feed;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.FragmentsRendering;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
